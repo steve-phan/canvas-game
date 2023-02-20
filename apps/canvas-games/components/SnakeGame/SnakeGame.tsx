e04bb2e65a2a6ffdee318335a44e766e8956f11a
Steve Phan <phanhaingoc@gmail.com>
2023-02-20 13:02:05 +0100

    WIP: addEventListener  keydown

--- apps/canvas-games/components/SnakeGame/SnakeGame.tsx
+++ apps/canvas-games/components/SnakeGame/SnakeGame.tsx
@@ -8,7 +8,11 @@ import {
   IObjectBody,
 } from '@canvas-games/shared/util-position';
 
-import { useAppSelector } from '../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import {
+  setDirection,
+  snakeDirections,
+} from '../../store/snakeGame/snakeGameSlice';
 
 /* eslint-disable-next-line */
 export interface SnakeGameProps {
@@ -25,16 +29,45 @@ export const SnakeGame = ({ width, height }: SnakeGameProps) => {
   const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
   // Get snake position from redux
   const snake = useAppSelector((state) => state.snakeGame.snake);
+  const dispatch = useAppDispatch();
   const [targetPos, setTargetPos] = useState<IObjectBody>(
     generateRandomPosition(width - 20, height - 20)
   );
+  const handleKeyDown = async (event: KeyboardEvent) => {
+    console.log({ event });
+    switch (event.code) {
+      case 'KeyW':
+        dispatch(setDirection(snakeDirections.UP));
+        break;
+      case 'KeyS':
+        dispatch(setDirection(snakeDirections.DOWN));
+        break;
+      case 'KeyA':
+        dispatch(setDirection(snakeDirections.LEFT));
+        break;
+      case 'KeyD':
+        dispatch(setDirection(snakeDirections.RIGHT));
+        break;
+      default:
+        break;
+    }
+  };
 
   useEffect(() => {
-    setContext(canvasRef.current && canvasRef.current.getContext('2d')); //
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
     drawObject(context, [targetPos], 'deepink'); //Draws target randomly
     drawObject(context, snake, 'red'); // Draws initial snake
   }, [context]);
 
+  useEffect(() => {
+    setContext(canvasRef.current && canvasRef.current.getContext('2d'));
+  }, []);
   return (
     <StyledSnakeGame>
       <h1>Welcome to SnakeGame!</h1>
